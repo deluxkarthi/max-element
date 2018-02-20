fe473b6f9e79b45cd76860e335ae9205d05d4fa6
deluxkarthi <35652981+deluxkarthi@users.noreply.github.com>
2018-02-20 16:46:08 +0530

    Create max.c

---
+++ max.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+  int a[10],temp;
+  int j;
+  for(j=0;j<10;i++)
+  { 
+    scanf("%d",&a[j]);
+  }
+  temp=a[0];
+  for(j=0;j<10;j++)
+  {
+    if(a[0]<a[j])
+    {
+      temp=a[j]);
+    }
+    printf("the largest number is %d",temp);
+  }
